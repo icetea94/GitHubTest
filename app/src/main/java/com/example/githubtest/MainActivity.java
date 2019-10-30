b871260c95cf9176f5dafdb7677ad1e43bfe79ce
icetea94 <57166268+icetea94@users.noreply.github.com>
2019-10-30 15:50:49 +0900

    Update MainActivity.java

--- app/src/main/java/com/example/githubtest/MainActivity.java
+++ app/src/main/java/com/example/githubtest/MainActivity.java
@@ -14,5 +14,6 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
         tv=findViewById(R.id.tv);
         tv.setText("성공");
+        tv.setText("NICE");
     }
 }
