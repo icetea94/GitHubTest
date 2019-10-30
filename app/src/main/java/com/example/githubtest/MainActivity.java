dbe6df00069340f5f1667fbff7bf25da7aa6c0ca
icetea94 <devshyeon@gmail.com>
2019-10-30 15:47:43 +0900

    first commit

--- app/src/main/java/com/example/githubtest/MainActivity.java
+++ app/src/main/java/com/example/githubtest/MainActivity.java
@@ -3,12 +3,16 @@ package com.example.githubtest;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+    TextView tv;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        tv=findViewById(R.id.tv);
+        tv.setText("성공");
     }
 }
